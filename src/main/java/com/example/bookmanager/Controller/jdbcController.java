106e7ea6415400381df425ac214acc5a22d59b49
hypeopal <D1557473239@outlook.com>
2025-01-14 19:45:24 +0800

    fix security problem

--- src/main/java/com/example/bookmanager/Controller/jdbcController.java
+++ src/main/java/com/example/bookmanager/Controller/jdbcController.java
@@ -5,9 +5,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Map;
-
 @RestController
 public class jdbcController {
     @Autowired
